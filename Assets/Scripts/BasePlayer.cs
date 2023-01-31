482f7fac9580a1eeb608f54b0c5c706f714ad648
Kurovah <45458052+Kurovah@users.noreply.github.com>
2023-01-31 23:14:47 +0000

    it's 3d now

--- Assets/Scripts/BasePlayer.cs
+++ Assets/Scripts/BasePlayer.cs
@@ -5,9 +5,11 @@ using UnityEngine;
 public class BasePlayer : MonoBehaviour
 {
     CharacterController character;
-    Vector3 velocity;
+    Vector3 velocity, actualVelocity;
     public float jumpheight, movespeed, gravity;
+    float lastYVel;
     bool grounded;
+    public Transform meshTransform;
     // Start is called before the first frame update
     void Start()
     {
@@ -18,7 +20,15 @@ public class BasePlayer : MonoBehaviour
     void Update()
     {
         grounded = Physics.Raycast(transform.position, Vector3.down, 0.01f, LayerMask.GetMask("Solid"));
-        velocity.x = Input.GetAxis("Horizontal") * movespeed * Time.deltaTime;
+        Vector3 rightVec = Vector3.Cross(-Camera.main.transform.forward, Vector3.up);
+        Vector3 forwardVec = Vector3.Cross(rightVec, Vector3.up);
+
+        Vector2 inputAxis = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+        velocity = rightVec * inputAxis.x  + forwardVec * inputAxis.y;
+        velocity  = velocity.normalized * movespeed * Time.deltaTime;
+        velocity.y = lastYVel;
+
+        
 
         if (grounded)
         {
@@ -30,8 +40,22 @@ public class BasePlayer : MonoBehaviour
                 
         }
 
-        velocity.y -= gravity * Time.deltaTime;
 
-        character.Move(velocity);
+
+        velocity.y -= gravity * Time.deltaTime;
+        actualVelocity = Vector3.Lerp(actualVelocity, velocity, 0.1f);
+        actualVelocity.y = velocity.y;
+        character.Move(actualVelocity);
+
+        if (inputAxis.magnitude > 0)
+            SetModelFacing(velocity);
+
+        lastYVel = velocity.y;
+    }
+
+    void SetModelFacing(Vector3 _facing)
+    {
+        _facing.y = 0;
+        meshTransform.forward = Vector3.Slerp(meshTransform.forward, _facing, 0.1f);
     }
 }
---
+++ Assets/Scripts/CameraBehaviour.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[ExecuteInEditMode]
+public class CameraBehaviour : MonoBehaviour
+{
+    public float zoom;
+    public Transform target, vcam;
+    public Vector3 offset;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        SetCamPos();
+    }
+
+    void SetCamPos()
+    {
+        transform.position = Vector3.Lerp(transform.position, target.position,.05f);
+        vcam.localPosition = new Vector3(0,0,-zoom);
+    }
+}
